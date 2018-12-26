45d13168abe98576e8c30f9bae24ff5c02a05c56
lijiajingtt <lijiajingtt@gmail.com>
2018-12-26 10:57:23 +0800

    登录界面

---
+++ src/main/java/com/soft1841/sm/SellerApp.java
@@ -0,0 +1,23 @@
+package com.soft1841.sm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SellerApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Login");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/seller.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
